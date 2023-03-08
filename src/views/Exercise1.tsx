ba28d36aa7e5ebff13d2f776c66cb0df215dffec
Francisco Javier Padron Luis <franciscopadron@capitole-consulting.com>
2023-03-08 18:35:23 +0100

    feat: :tada: initial commit
    Create Empty project

---
+++ src/views/Exercise1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Exercise1() {
+  return (
+    <div>Exercise1</div>
+  )
+}
---
+++ src/views/Exercise2.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Exercise2() {
+  return <div>Exercise2</div>;
+}
