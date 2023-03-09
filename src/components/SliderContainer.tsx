785d4bb5431af3fc56eaf90a5095f427783e9ac3
Francisco Javier Padron Luis <franciscopadron@capitole-consulting.com>
2023-03-09 12:33:26 +0100

    feat: :art: add set value for inputs
    Add set value for inputs and some styles

--- src/components/SliderContainer.tsx
+++ src/components/SliderContainer.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Slider from "./Slider";
-import "./slider_container.css";
+import SliderInput from "./SliderInput";
+import "./slider-container.css";
 
 export default function SliderContainer() {
   return (
     <div className="container">
-      <div className="default_box label_left">2€</div>
+      <div className="default_box label_left">
+        <SliderInput value={2} />
+      </div>
       <div className="default_box slide">
         <Slider />
       </div>
-      <div className="default_box label_right">10€</div>
+      <div className="default_box label_right">
+        <SliderInput value={10} />
+      </div>
     </div>
   );
 }
