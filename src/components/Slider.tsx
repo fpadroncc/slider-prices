7fb87900dff34c79a4b3044151b5cec879e6035e
Francisco Javier Padron Luis <franciscopadron@capitole-consulting.com>
2023-03-13 11:07:51 +0100

    feat: :construction: control slide box

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import useMouseController from "../hooks/useMouseController";
+import useMouseController, { TBullet } from "../hooks/useMouseController";
 import "./slider.css";
 
 export default function Slider({
@@ -9,52 +9,12 @@ export default function Slider({
   bulletRight: number;
   bulletLeft: number;
 }) {
-  const minValue = useMouseController(bulletLeft)
-  //const [minValue, setMinValue] = useState<number>(width);
-  const [maxValue, setMaxValue] = useState<number>(bulletRight);
-
-  const [isGrabBullet, setGrabBullet] = useState<boolean>(false);
-  const [bulletSelected, setBulletSelected] = useState<string | null>(null);
-
-  const moveObject = (event: globalThis.MouseEvent) => {
-    const container = document.getElementById("containerSlide");
-
-    const moveHorizontally = () => {
-      if (container) {
-        //1º Get the bullet line size: container?.clientWidth
-        const getLineLong = container.clientWidth;
-
-        //2º Get Cursor Position on Bullet Line and equal to size
-        const cursorPosition = event.offsetX;
-
-        //3º Math equal widht with position of cursor
-        // Bullet line size is equal to 100%
-        // Cursor 0 is equal to 0% and cursor on end of the line x is 100%
-        const getPorcent = Math.round((cursorPosition / getLineLong) * 100);
-
-        //4º Rest 5% for center the point on the cursor click and setted
-        if (bulletSelected === "rightBullet") {
-          //setMaxValue(getPorcent - 5);
-        }
-
-        if (bulletSelected === "leftBullet") {
-          //setMinValue(getPorcent - 5);
-        }
-
-        // const customTable = {
-        //   sizeLine: getLineLong,
-        //   cursor: cursorPosition,
-        //   getPorcent: getPorcent,
-        // };
-
-        // console.table(customTable);
-
-        return getPorcent;
-      }
-    };
-
-    moveHorizontally();
-  };
+  const [bulletSelected, setBulletSelected] = useState<TBullet>(null);
+  const controlValue = useMouseController(
+    100,
+    "containerSlide",
+    bulletSelected
+  );
 
   return (
     <div id="containerSlide" className="container_slide">
@@ -62,17 +22,17 @@ export default function Slider({
         <div
           id="leftBullet"
           className="bullet left_bullet"
-          style={{ left: `${minValue}%` }}
+          style={{ left: `${controlValue}%` }}
           onMouseDown={(e) => {
-            setBulletSelected("leftBullet");
+            setBulletSelected("LEFT_BULLET");
           }}
         ></div>
         {/* <div
           id="rightBullet"
           className="bullet right_bullet"
-          style={{ left: `${maxValue}%` }}
+          style={{ left: `${controlValue}%` }}
           onMouseDown={() => {
-            setBulletSelected("rightBullet");
+            setBulletSelected("RIGH_BULLET");
           }}
         ></div> */}
       </div>
--- src/hooks/useMouseController.tsx
+++ src/hooks/useMouseController.tsx
@@ -1,36 +1,44 @@
 import React, { useState, useCallback, useEffect, useRef } from "react";
 
-const useMouseController = (initialWidth: number) => {
+export type TBullet = "RIGH_BULLET" | "LEFT_BULLET" | null;
+
+const useMouseController = (
+  initialWidth: number,
+  longContainer: string,
+  bulletSelected: TBullet
+) => {
+  const [dragging, setDragging] = useState(false);
   const [result, setResult] = useState(initialWidth);
+  const [currentWidth, setCurrentWidth] = useState(initialWidth);
 
-  const dragging = useRef(false);
-  const previousClientX = useRef(0);
-
-  const handleMouseMove = useCallback((e: globalThis.MouseEvent) => {
-    if (!dragging.current) {
-      return;
-    }
-
-    setResult((prevValue) => {
-      const change = e.clientX - previousClientX.current;
-      previousClientX.current = e.clientX;
-      return prevValue + change;
-    });
-  }, []);
+  const origin = useRef(initialWidth);
 
   const handleMouseDown = useCallback((e: globalThis.MouseEvent) => {
     console.log("handleMouseDown");
     document.body.style.cursor = "grabbing";
-    previousClientX.current = e.clientX;
-    dragging.current = true;
+    origin.current = e.clientX;
+    //dragging.current = true;
+    setDragging(true);
   }, []);
 
   const handleMouseUp = useCallback(() => {
     console.log("handleMouseUp");
     document.body.style.cursor = "auto";
+    setDragging(false);
+    setCurrentWidth(result);
+    //dragging.current = false;
+  }, [result]);
 
-    dragging.current = false;
-  }, []);
+  const handleMouseMove = useCallback(
+    (e: globalThis.MouseEvent) => {
+      if (!dragging) {
+        return;
+      }
+
+      moveHorizontally(e);
+    },
+    [currentWidth, dragging]
+  );
 
   useEffect(() => {
     window.addEventListener("mousedown", handleMouseDown);
@@ -42,7 +50,43 @@ const useMouseController = (initialWidth: number) => {
       window.removeEventListener("mouseup", handleMouseUp);
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, [handleMouseDown, handleMouseUp, handleMouseMove]);
+  }, [dragging, handleMouseDown, handleMouseUp, handleMouseMove]);
+
+  const moveHorizontally = (event: globalThis.MouseEvent) => {
+    const container = document.getElementById(`${longContainer}`);
+    if (container) {
+      //1º Get the bullet line size: container?.clientWidth
+      const getLineLong = container.clientWidth;
+
+      //2º Get Cursor Position on Bullet Line and equal to size
+      //const cursorPosition = event.offsetX;
+      const cursorPosition = event.clientX;
+
+      //3º Math equal widht with position of cursor
+      // Bullet line size is equal to 100%
+      // Cursor 0 is equal to 0% and cursor on end of the line x is 100%
+      const getPorcent = Math.round((cursorPosition / getLineLong) * 100);
+
+      //4º Rest 5% for center the point on the cursor click and setted
+      if (bulletSelected === "RIGH_BULLET") {
+        //setMaxValue(getPorcent - 5);
+      }
+
+      if (bulletSelected === "LEFT_BULLET") {
+      }
+
+      // console.log("currentWidth", currentWidth);
+      // console.log("clientX", event.clientX);
+      // console.log("origin", origin.current);
+      // console.log("sum", currentWidth + event.clientX);
+
+      const operation = currentWidth + event.clientX - origin.current;
+      const checkLimits =
+        operation >= 100 ? 95 : operation <= 0 ? 0 : operation;
+
+      setResult(() => checkLimits);
+    }
+  };
 
   return result;
 };
